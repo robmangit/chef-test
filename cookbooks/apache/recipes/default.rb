97cd8faf248acc2636c4ffc3bc50ca556ffa1446
robmangit <rmanoughian@gmail.com>
2018-03-10 14:27:03 +0000

    fixing my shitty syntax

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
-if node ["platform_family"] == "rhel"
+if node ['platform_family'] == "rhel"
   package = "httpd" 
-elsif node ["platform_family"] == "debian"
+elsif node ['platform_family'] == "debian"
   package = "apache2" 
 end
 
